8faf5778829101d0d30d90e928d5104d98806ee7
Fabian Bornhofen <fbornhofen@googlemail.com>
2011-10-11 10:54:35 -0700

    init

---
+++ codesrv.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('/home/fb/sw/codesrv/codedb.sqlite3');
+
+var app = express.createServer();
+
+// logic
+
+function setupDb() {
+  db.exec("DROP TABLE IF EXISTS files; CREATE TABLE files " +
+          "(path string, revision int, timestamp int, user int, contents blob)");
+}
+
+function getRevision(revNr, continuation) {
+  db.serialize(function() {
+    db.all("SELECT DISTINCT(path) FROM files WHERE revision = " + revNr, function(err, dbres) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      continuation(dbres);
+    });
+  });
+}
+
+function getFile(fileName, revNr, continuation) {
+  db.serialize(function() {
+    console.log('getting ' + fileName + '(' + revNr + ')');
+    db.all("SELECT * FROM files WHERE path like '" + fileName + "' AND revision = " + revNr, function (err, dbres) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      continuation(dbres);
+    });
+  });
+}
+
+// controller
+
+app.get('/setup', function(req, res) {
+  setupDb();
+  res.send('check if db has been set up successfully');
+});
+
+app.get('/:rev', function(req, res) {
+  getRevision(req.params.rev, function(dbres) {
+    var resStr = ''
+    resStr = JSON.stringify(dbres);
+    res.send(resStr);
+  });
+});
+
+app.get(/^\/(\d+)\/(.*)/, function(req, res) {
+  var rev = req.params[0],
+      file = req.params[1];
+  getFile(file, rev, function(dbres) {
+    if (dbres[0]) {
+      res.send(dbres[0]['contents']);
+    } else {
+      res.send('');
+      return;
+    } 
+  });
+});
+
+app.listen(3000);
