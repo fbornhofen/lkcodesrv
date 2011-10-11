1d2c5353c12eac478c0568db9c8779333ceb6c31
Fabian Bornhofen <fbornhofen@googlemail.com>
2011-10-11 14:10:28 -0700

    curl for import

--- import-code.sh
+++ import-code.sh
@@ -6,7 +6,7 @@ LK_CODEDB="livelydb.sqlite3"
 cd $1
 for f in `find . -type f | grep -v '.git/'`; do
   f=${f:2}
-  curl -d @$f http://localhost:3000/$f
+  curl --data-binary @$f http://localhost:3000/$f
   echo $f;
 done
 
