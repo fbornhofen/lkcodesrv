6104baaecda055bc8510b79ac2d0289af9b1a033
Fabian Bornhofen <fbornhofen@googlemail.com>
2011-10-17 10:12:57 -0700

    integrated log4js

--- codesrv.js
+++ codesrv.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var sqlite3 = require('sqlite3');
-//var mime = require('mime');
-//var libxml = require('libxmljs');
+var log4js = require('log4js');
 var dbPath = process.argv[2];
 var port = 80;
 
-console.log('using database ' + dbPath);
+var logger = log4js.getLogger();
+logger.setLevel('info');
+
+logger.info('using database ' + dbPath);
 var db = new sqlite3.Database(dbPath);
 
 var app = express.createServer();
 
-var codeDatabase = require('./lib/codedatabase.js')(db);
-var controller = require('./lib/controller.js')(codeDatabase);
+var codeDatabase = require('./lib/codedatabase.js')(db, logger);
+var controller = require('./lib/controller.js')(codeDatabase, logger);
 require('./lib/routes.js')(app, controller);
 
 app.listen(port);
