341f571518bf2f13b147b70733c9bf23e7b329df
Fabian Bornhofen <fbornhofen@googlemail.com>
2011-10-19 14:10:43 -0700

    proxy route (for Lively WebResource)

--- codesrv.js
+++ codesrv.js
@@ -1,14 +1,12 @@
 var express = require('express');
-//var sqlite3 = require('sqlite3');
 var DBWrapper = require('node-dbi').DBWrapper;
 var log4js = require('log4js');
 var config = require('./config');
-//var dbPath = process.argv[2];
 
 var logger = log4js.getLogger();
 logger.setLevel(config.logLevel);
 
-//var db = new sqlite3.Database(dbPath);
+
 var db = new DBWrapper('sqlite3', config.dbConfig);
 db.connect(function(arg) {
   logger.info('db ' + (db._connected?'connected':'NOT connected'));
@@ -17,8 +15,11 @@ logger.info('using database ' + config.dbConfig.path);
 
 var app = express.createServer();
 
+var proxy = require('./lib/proxy.js')(logger);
 var codeDatabase = require('./lib/codedatabase.js')(db, logger);
 var controller = require('./lib/controller.js')(codeDatabase, logger);
-require('./lib/routes.js')(app, controller, logger);
+
+require('./lib/routes.js')(app, controller, logger, proxy);
 
 app.listen(config.port);
+
