6045a202b8e2a406dbf48071151b40530100bd72
Lauritz Thamsen <l.thamsen@googlemail.com>
2011-10-20 19:46:58 -0700

    more options for import script

--- import-code.sh
+++ import-code.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
 LK_PATH=$1
-LK_PORT=$2
+LK_SERVER_HOST=$2
+LK_SERVER_PORT=$3
 LK_CODEDB="livelydb.sqlite3"
 
-if [ "$LK_PORT" = "" ]; then
-  LK_PORT=8888
+if [ "$LK_SERVER_HOST" = "" ]; then
+  LK_SERVER_HOST="localhost"
+fi
+
+if [ "$LK_SERVER_PORT" = "" ]; then
+  LK_SERVER_PORT=8888
 fi
 
 cd $LK_PATH
 for f in `find . -type f | grep -v '.git/'`; do
   f=${f:2}
-  curl --data-binary @$f http://localhost:$LK_PORT/$f
+  curl --data-binary @$f http://$LK_SERVER_HOST:$LK_SERVER_PORT/$f
   echo $f;
 done
 
